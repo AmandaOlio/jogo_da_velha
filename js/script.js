e572bf5a844d79823ec94ff1844b4b914c800e91
Amandinha <amandaaa.b.oliveira@gmail.com>
2023-05-31 11:54:23 -0300

    commit update

--- js/script.js
+++ js/script.js
@@ -14,10 +14,10 @@ let finalJogo = false;
 
 //Click botão 1
 function clickBtn1() {
-    jogadas++;
     let btn = document.getElementById('btn1');
 
     if (btn.value == " " && !finalJogo) {
+        jogadas++;
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
@@ -33,10 +33,10 @@ function clickBtn1() {
 }
 
 function clickBtn2() {
-    jogadas++;
     let btn = document.getElementById('btn2');
 
     if (btn.value == " " && !finalJogo) {
+        jogadas++;
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
@@ -53,10 +53,10 @@ function clickBtn2() {
 }
 
 function clickBtn3() {
-    jogadas++;
     let btn = document.getElementById('btn3');
 
     if (btn.value == " " && !finalJogo) {
+        jogadas++;
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
@@ -72,10 +72,10 @@ function clickBtn3() {
 }
 
 function clickBtn4() {
-    jogadas++;
     let btn = document.getElementById('btn4');
 
     if (btn.value == " " && !finalJogo) {
+        jogadas++;
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
@@ -91,10 +91,10 @@ function clickBtn4() {
 }
 
 function clickBtn5() {
-    jogadas++;
     let btn = document.getElementById('btn5');
 
     if (btn.value == " " && !finalJogo) {
+        jogadas++;
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
@@ -110,10 +110,10 @@ function clickBtn5() {
 }
 
 function clickBtn6() {
-    jogadas++;
     let btn = document.getElementById('btn6');
 
     if (btn.value == " " && !finalJogo) {
+        jogadas++;
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
@@ -129,10 +129,10 @@ function clickBtn6() {
 }
 
 function clickBtn7() {
-    jogadas++;
     let btn = document.getElementById('btn7');
 
     if (btn.value == " " && !finalJogo) {
+        jogadas++;
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
@@ -148,10 +148,10 @@ function clickBtn7() {
 }
 
 function clickBtn8() {
-    jogadas++;
     let btn = document.getElementById('btn8');
 
     if (btn.value == " " && !finalJogo) {
+        jogadas++;
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
@@ -167,10 +167,10 @@ function clickBtn8() {
 }
 
 function clickBtn9() {
-    jogadas++;
     let btn = document.getElementById('btn9');
 
     if (btn.value == " " && !finalJogo) {
+        jogadas++;
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
@@ -301,5 +301,55 @@ function verificaGanhador() {
         return;
       }
 
+      if(controle==0) {
+        lblJogador.innerText = 'Jogador X';
+      } else {
+        lblJogador.innerText = 'Jogador O';
+      }
+
       lblJogadas.innerText = '(' + (jogadas+1) + 'ª Jogada)';
+}
+
+//Click do botão Reiniciar
+function clickReiniciar() {
+    btn1 = 0;
+    btn2 = 0;
+    btn3 = 0;
+    btn4 = 0;
+    btn5 = 0;
+    btn6 = 0;
+    btn7 = 0;
+    btn8 = 0;
+    btn9 = 0;
+    controle = 0;
+    jogadas = 0;
+    finalJogo = false;
+
+    let b1 = document.getElementById('btn1');
+    let b2 = document.getElementById('btn2');
+    let b3 = document.getElementById('btn3');
+    let b4 = document.getElementById('btn4');
+    let b5 = document.getElementById('btn5');
+    let b6 = document.getElementById('btn6');
+    let b7 = document.getElementById('btn7');
+    let b8 = document.getElementById('btn8');
+    let b9 = document.getElementById('btn9');
+
+    b1.value = ' ';
+    b2.value = ' ';
+    b3.value = ' ';
+    b4.value = ' ';
+    b5.value = ' ';
+    b6.value = ' ';
+    b7.value = ' ';
+    b8.value = ' ';
+    b9.value = ' ';
+
+    //Reiniciar Label
+    let lblJogador = document.getElementById('lblJogador');
+    let lblJogadas = document.getElementById('lblJogadas');
+
+    lblJogador.innerText = 'Jogador X';
+    lblJogadas.innerText = '(1ª Jogada)';
+
 }
